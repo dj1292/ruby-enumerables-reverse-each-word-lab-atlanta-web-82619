cd5c095e1b9f23e1617fee8b49b03a46ccccc768
Dan Reeves <djreeves1292@gmail.com>
2019-08-21 19:07:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   newArr = string.split(' ')
   new_arr = []
  newArr.each do |string|
-   new_arr << newArr
+   new_arr << newArr.unshift
  end
    return new_arr
 end
