cf041c439ca751490a7574cf88cb6fa2e2ffc902
Dan Reeves <djreeves1292@gmail.com>
2019-08-21 19:14:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,9 @@ def reverse_each_word(string)
   newArr = string.split('')
   array = []
   newArr.each do |string|
-    newArr.unshift
-    
-  end 
- return newArr
+  array << newArr.unshift
+    end 
+ return array
  end
   
     
