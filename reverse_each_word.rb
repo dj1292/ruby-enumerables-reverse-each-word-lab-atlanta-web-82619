da1d182f0843f5db4e1b93b3f624a017e45d5a87
Dan Reeves <djreeves1292@gmail.com>
2019-08-21 18:45:47 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(string)
+  newArr = string.split('')
+  ele = newArr[1..-1]
+  new_arr = []
+ newArr.each do |string|
+   new_arr << newArr
+ end
+   return new_arr
+end
+    
+    
